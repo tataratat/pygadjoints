b40bb6e050049910c3966bc736a3dfa0c096ff90
jzwar <zwar@ilsb.tuwien.ac.at>
2023-08-03 18:08:42 +0200

    Create really basic example. Why does it not work

--- src/pygadjoints/py_elasticity.hpp
+++ src/pygadjoints/py_elasticity.hpp
@@ -1,18 +1,21 @@
-#include
+#include <gismo.h>
 
-// namespace and stuff
+namespace test::printer {
+class Printer {
+ public:
+  Printer() = default;
 
-
-
-
-template<typename Type>
-void add_adjoint_class(py::module& m, const char* class_name) {
-  //add general class attribs
-
-  py::class_</* cpp class comes here. z.b. Type */> klasse(m, class_name);
-
-  klasse.def(py::init<>())
-    .def("");
-    
+  print(const int i) {
+    for (int j{}; j < i; i++) {
+      GISMO_INFO("TEST IF GISMO IS THERE SUCCEEDED")
+    };
+  }
 }
 
+template <typename Type>
+void add_adjoint_class(py::module& m, const char* class_name) {
+  py::class_<test::printer::Printer> klasse(m, class_name);
+  klasse.def(py::init<>())
+      .def("print", &test::printer::print, py::arg("int");
+}
+}  // namespace test::printer
