e40d551a9db05b1a724b56bbb805dad3a4b34d5a
jzwar <jacqueszwar@gmail.com>
2024-03-06 11:29:24 +0200

    Plot iterations

--- examples/Poisson/Optimization/2D-Rectangle/plot_iterations.py
+++ examples/Poisson/Optimization/2D-Rectangle/plot_iterations.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
-array = np.genfromtxt("log_file_iterations.csv", delimiter=",")
-plt.subplot(2, 1, 1)
-plt.plot(array[:, 1].flat)
+iterations = np.genfromtxt("log_file_iterations.csv", delimiter=",")
+sensitivities = np.genfromtxt("log_file_sensitivities.csv", delimiter=",")
+n_params = int((sensitivities.shape[1]-2)/2)
+
+plt.subplot(3, 1, 1)
+plt.plot(iterations[:, 1].flat, '-o')
 plt.title('Evolution of the Objective function')
 plt.ylabel('Objective Funtion')
-step_size = np.linalg.norm(np.diff(array[:, 2:], axis=0), axis=1)
+step_size = np.linalg.norm(np.diff(iterations[:, 2:], axis=0), axis=1)
 
-plt.subplot(2, 1, 2)
+
+plt.subplot(3, 1, 2)
 plt.semilogy(np.hstack((step_size, [0])))
 plt.xlabel('Iteration')
+plt.ylabel('Step size')
+
+plt.subplot(3, 1, 3)
+plt.plot(sensitivities[:, 0], np.linalg.norm(
+    sensitivities[:, 2:(2+n_params)], axis=1), '-x')
+plt.xlabel('Norm of sensitivities')
 plt.ylabel('Step size')
 
 plt.show()
